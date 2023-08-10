398288d6ec6199a85571cfe4d6994cd1dcde3b6d
MinjiY <upi907@naver.com>
2023-08-11 00:05:33 +0900

    feat: 대댓글 엔티티 생성

---
+++ src/main/java/trip/community/model/Reply.java
@@ -0,0 +1,25 @@
+package trip.community.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "reply")
+public class Reply extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long replyId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    private String content;
+
+    private String type;
+
+    private Long views;
+
+    private Long likeCnt;
+
+}
