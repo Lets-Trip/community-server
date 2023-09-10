04e3b2c1ed203966e5cd87c85cd54dbd74c0ab05
minji_yoo <upi907@naver.com>
2023-09-10 21:56:00 +0900

    feat: cors

--- src/main/java/trip/community/global/config/ServerConfig.java
+++ src/main/java/trip/community/global/config/ServerConfig.java
@@ -1,10 +1,20 @@
 package trip.community.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ServerConfig implements WebMvcConfigurer {
     public static final String CURRENT_API_URI = "/api/v1";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000/")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+        ;
+    }
 
 }
